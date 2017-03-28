9cedb373960b1733e28ecc7fadf748fd00a781c8
sschaub <sschaub@bju.edu>
2017-03-28 14:25:11 -0400

    Blend?

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             // Hello, wonderful you!
+            Console.WriteLine("Will it blend?");
         }
     }
 }
