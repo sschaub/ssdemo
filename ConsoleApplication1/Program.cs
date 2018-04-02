8b6ae212a5e0d4356811735de27c2db8e324d4c3
sschaub <sschaub@gmail.com>
2018-04-02 13:13:15 -0400

    Update Program.cs

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            string name;
             Console.WriteLine("Will it swirl?");
         }
         
