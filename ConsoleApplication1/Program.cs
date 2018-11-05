3edc7c07f62d22182709b78cde4af2e4c188547e
Stephen Schaub <sschaub@gmail.com>
2018-11-05 14:29:43 -0500

    Update name

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             // Here is a name variable
-            string name;
+            string someName;
         }
         
         static int Harmless(int x)
