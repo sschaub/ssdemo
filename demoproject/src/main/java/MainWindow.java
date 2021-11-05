db5a67ad1bc209124241b71bcb9f23ccad3acd41
Stephen Schaub <sschaub@bju.edu>
2021-11-05 14:17:11 -0400

    New comment

--- demoproject/src/main/java/MainWindow.java
+++ demoproject/src/main/java/MainWindow.java
@@ -9,7 +9,7 @@ public class MainWindow {
     
     int count = 1;  // Initialize count
 
-    int turkeyWeight = 36;
+    int turkeyWeight = 36; // increment each time we buy a new turkey
 
     @FXML
     void onGreetClicked(ActionEvent event) {
